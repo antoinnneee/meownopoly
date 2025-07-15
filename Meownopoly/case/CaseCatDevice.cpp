726b1b87a1660179ae7f56e14524137cc92b5ffd
Valere21 <valere.gh@hotmail.fr>
2025-07-15 21:47:26 +0200

    add stuff

--- Meownopoly/case/CaseCatDevice.cpp
+++ Meownopoly/case/CaseCatDevice.cpp
@@ -1,14 +1,14 @@
 #include "CaseCatDevice.h"
 #include "../player.h"
 
-CaseCatDevice::CaseCatDevice(QObject *parent)
-    : CaseCatPerks("Unknown Rest Area", -1, parent)
-{
-    setType(CT_Device);
-}
+// CaseCatDevice::CaseCatDevice(QObject *parent)
+//     : CaseCatPerks("Unknown Cat Device", -1, morgagePrice, parent)
+// {
+//     setType(CT_Device);
+// }
 
-CaseCatDevice::CaseCatDevice(const QString &name, int position, QObject *parent)
-    : CaseCatPerks(name, position, parent)
+CaseCatDevice::CaseCatDevice(CASECATPERKS_DEFAULT_PARAMETER_NOP, int taxe)
+    : CASECATPERKS_DEFAULT_CONSTRUCT_PARAMETER, m_taxe(taxe)
 {
     setType(CT_Device);
 }
@@ -28,3 +28,16 @@ bool CaseCatDevice::sellCase(Player *buyer)
 }
 
 
+
+int CaseCatDevice::taxe() const
+{
+    return m_taxe;
+}
+
+void CaseCatDevice::setTaxe(int newTaxe)
+{
+    if (m_taxe == newTaxe)
+        return;
+    m_taxe = newTaxe;
+    emit taxeChanged();
+}
--- Meownopoly/case/CaseCatPerks.h
+++ Meownopoly/case/CaseCatPerks.h
@@ -4,18 +4,26 @@
 #include <QObject>
 #include "case/Case.h"
 
+#define CASECATPERKS_DEFAULT_PARAMETER const QString &name, int position = -1, int morgagePrice = -1, int price = -1, int sellPrice = -1, QObject *parent = nullptr
+#define CASECATPERKS_DEFAULT_PARAMETER_NOP const QString &name, int position, int morgagePrice, int price, int sellPrice, QObject *parent
+#define CASECATPERKS_DEFAULT_CONSTRUCT_PARAMETER CaseCatPerks(name, position, price, sellPrice, morgagePrice, parent)
+
+
 class Player;
+
+
 
 class CaseCatPerks : public Case
 {
     Q_OBJECT
     Q_PROPERTY(int price READ price WRITE setPrice NOTIFY priceChanged FINAL)
     Q_PROPERTY(int sellPrice READ sellPrice WRITE setsellPrice NOTIFY sellPriceChanged FINAL)
+    Q_PROPERTY(int morgagePrice READ morgagePrice WRITE setmorgagePrice NOTIFY morgagePriceChanged FINAL)
 
 
 public:
 
-    CaseCatPerks(const QString &name, int position = -1, QObject *parent = nullptr);
+    CaseCatPerks(CASECATPERKS_DEFAULT_PARAMETER);
 
 
     bool buyCase(Player *buyer);
@@ -29,16 +37,22 @@ public:
     void setsellPrice(int newSellPrice);
 
 
+    int morgagePrice() const;
+    void setmorgagePrice(int newMorgagePrice);
+
 signals:
     void priceChanged();
 
     void sellPriceChanged();
+
+    void morgagePriceChanged();
 
 protected:
 
     Player *owner;
     int m_price;
     int m_sellPrice;
+    int m_morgagePrice;
 };
 
 #endif // CASECATPERKS_H
