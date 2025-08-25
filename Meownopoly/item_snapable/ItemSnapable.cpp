ad786a8ec777b3d07d17417a56248d7240b93fa2
antoinnneee <antoinnneee@gmail.com>
2025-08-25 13:06:25 +0200

    itemSnapable first step

--- Meownopoly/item_snapable/ItemSnapable.cpp
+++ Meownopoly/item_snapable/ItemSnapable.cpp
@@ -5,5 +5,67 @@ ItemSnapable::ItemSnapable() {}
 void ItemSnapable::registerQml()
 {
     qmlRegisterType<ItemSnapable>("ItemSnapable", 1, 0, "ItemSnapable"); // Register ItemSnapable class
+
+}
+
+ItemSnapable::ItemSnapable(Case * caseData, DisplayParameter * displayParameter, QObject *parent)
+: QObject(parent)
+{
+    m_caseData = caseData;
+    m_displayParameter = displayParameter;
+}
+
+DisplayParameter::DisplayParameter(int unitSizeWidth, int unitSizeHeight, int gridRelativePosition, int gridRelativePositionY, int zLayer, QObject *parent)
+: QObject(parent)
+{
+    m_unitSizeWidth = unitSizeWidth;
+    m_unitSizeHeight = unitSizeHeight;
+    m_gridRelativePosition = gridRelativePosition;
+    m_gridRelativePositionY = gridRelativePositionY;
+    m_zLayer = zLayer;
+}
+
+DisplayParameter::DisplayParameter(const QJsonDocument &json, QObject *parent): QObject(parent)
+{
+    m_json = json;
+    m_unitSizeWidth = m_json["unitSizeWidth"].toInt();
+    m_unitSizeHeight = m_json["unitSizeHeight"].toInt();
+    m_gridRelativePosition = m_json["gridRelativePosition"].toInt();
+    m_gridRelativePositionY = m_json["gridRelativePositionY"].toInt();
+    m_zLayer = m_json["zLayer"].toInt();
+}
+
+
+
+ItemSnapable::ItemSnapable(const QJsonDocument &json)
+{
+    m_json = json;
+    m_caseData = new Case(m_json["caseData"]);
+    m_displayParameter = new DisplayParameter(m_json["displayParameter"], this);
+    emit caseDataChanged();
+    emit displayParameterChanged();
+}
+
+QString ItemSnapable::toJSON()
+{
+    QString json;
+    json += "{\n";
+    json += "    \"caseData\": " + m_caseData->toJSON() + ",\n";
+    json += "    \"displayParameter\": " + m_displayParameter->toJSON() + "\n";
+    json += "}";
+    return json;
+}
+
+QString DisplayParameter::toJSON()
+{
+    QString json;
+    json += "{\n";
+    json += "    \"unitSizeWidth\": " + QString::number(m_unitSizeWidth) + ",\n";
+    json += "    \"unitSizeHeight\": " + QString::number(m_unitSizeHeight) + ",\n";
+    json += "    \"gridRelativePosition\": " + QString::number(m_gridRelativePosition) + ",\n";
+    json += "    \"gridRelativePositionY\": " + QString::number(m_gridRelativePositionY) + ",\n";
+    json += "    \"zLayer\": " + QString::number(m_zLayer) + "\n";
+    json += "}";
+    return json;
 }
 
--- Meownopoly/item_snapable/ItemSnapable.h
+++ Meownopoly/item_snapable/ItemSnapable.h
@@ -5,23 +5,84 @@
 #include <QQmlEngine>
 #include <QUrl>
 #include "case/Case.h"
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonValue>
+#include <QJsonValueRef>
+#include <QJsonValueRef>
+
+class DisplayParameter: public QObject {
+    Q_OBJECT
+    public:
+        Q_PROPERTY(int unitSizeWidth READ unitSizeWidth WRITE setUnitSizeWidth NOTIFY unitSizeWidthChanged)
+        Q_PROPERTY(int unitSizeHeight READ unitSizeHeight WRITE setUnitSizeHeight NOTIFY unitSizeHeightChanged)
+        Q_PROPERTY(int gridRelativePosition READ gridRelativePosition WRITE setGridRelativePosition NOTIFY gridRelativePositionChanged)
+        Q_PROPERTY(int gridRelativePositionY READ gridRelativePositionY WRITE setGridRelativePositionY NOTIFY gridRelativePositionYChanged)
+        Q_PROPERTY(int zLayer READ zLayer WRITE setZLayer NOTIFY zLayerChanged)
+
+        DisplayParameter();
+        DisplayParameter(int unitSizeWidth = 0, int unitSizeHeight = 0, int gridRelativePosition = 0, int gridRelativePositionY = 0, int zLayer = 5, QObject *parent = nullptr);
+        DisplayParameter(const QJsonDocument &json, QObject *parent = nullptr);
+        QString toJSON();
+
+        int unitSizeWidth() const {return m_unitSizeWidth;}
+        void setUnitSizeWidth(int unitSizeWidth) {m_unitSizeWidth = unitSizeWidth; emit unitSizeWidthChanged();}
+        int unitSizeHeight() const {return m_unitSizeHeight;}
+        void setUnitSizeHeight(int unitSizeHeight) {m_unitSizeHeight = unitSizeHeight; emit unitSizeHeightChanged();}
+        int gridRelativePosition() const {return m_gridRelativePosition;}
+        void setGridRelativePosition(int gridRelativePosition) {m_gridRelativePosition = gridRelativePosition; emit gridRelativePositionChanged();}
+        int gridRelativePositionY() const {return m_gridRelativePositionY;}
+        void setGridRelativePositionY(int gridRelativePositionY) {m_gridRelativePositionY = gridRelativePositionY; emit gridRelativePositionYChanged();}
+        int zLayer() const {return m_zLayer;}
+        void setZLayer(int zLayer) {m_zLayer = zLayer; emit zLayerChanged();}
+
+
+        signals:
+            void unitSizeWidthChanged();
+            void unitSizeHeightChanged();
+            void gridRelativePositionChanged();
+            void gridRelativePositionYChanged();
+            void zLayerChanged();
+
+        private :
+            int m_unitSizeWidth;
+            int m_unitSizeHeight;
+            int m_gridRelativePosition;
+            int m_gridRelativePositionY;
+            int m_zLayer;
+    
+
+};
 
 class ItemSnapable : public QObject
 {
     Q_OBJECT
 public:
     ItemSnapable();
+    ItemSnapable(Case * caseData, DisplayParameter * displayParameter);
+    ItemSnapable(const QJsonDocument &json);
+
+    Case * caseData() const {return m_caseData;}
+    void setCaseData(Case * caseData) {m_caseData = caseData; emit caseDataChanged();}
+    DisplayParameter * displayParameter() const {return m_displayParameter;}
+    void setDisplayParameter(DisplayParameter * displayParameter) {m_displayParameter = displayParameter; emit displayParameterChanged();}
+
+    signals:
+        void caseDataChanged();
+        void displayParameterChanged();
 
     static void registerQml();
 
-private :
+    Q_INVOKABLE QString toJSON();
+    
 
-    int unitSizeWidth;
-    int unitSizeHeight;
-    int gridRelativePosition;
-    int gridRelativePositionY;
-    int zLayer;
-    QUrl assetUrl;
+
+
+    private :
+        Case * m_caseData;
+        DisplayParameter * m_displayParameter;
+
 };
 
 #endif // ITEMSNAPABLE_H
