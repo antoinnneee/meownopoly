527c55de9b366bbcf914fab077818ae660e8acbf
Antoine bureau <bureauAntoine@cb4tech.com>
2025-09-03 11:40:46 +0200

    Add deletion options to decompressFolder method
    Introduces FolderCompressorDeleteOptions enum to control deletion of source and/or destination during decompression. Updates decompressFolder to accept these options and applies them accordingly. Refactors launcher_manager to use the new options, enabling automatic cleanup of files and folders during asset extraction.

--- Meownopoly/QtFolderCompressor/FolderCompressor.h
+++ Meownopoly/QtFolderCompressor/FolderCompressor.h
@@ -6,6 +6,14 @@
 #include <QDir>
 #include <QtQml/qqml.h>
 
+// Enum for deletion options using bit flags
+enum FolderCompressorDeleteOptions {
+    FC_DELETE_NONE = 0x00,                    // No deletion
+    FC_DELETE_SOURCE = 0x01,                  // Delete source file after decompression
+    FC_DELETE_DESTINATION = 0x02,             // Delete destination folder before decompression
+    FC_DELETE_BOTH = FC_DELETE_SOURCE | FC_DELETE_DESTINATION  // Delete both
+};
+
 class FolderCompressor : public QObject
 {
     Q_OBJECT
@@ -20,7 +28,7 @@ public:
 
     //A function that deserializes data from the compressed file and
     //creates any needed subfolders before saving the file
-    Q_INVOKABLE bool decompressFolder(QString sourceFile, QString destinationFolder);
+    Q_INVOKABLE bool decompressFolder(QString sourceFile, QString destinationFolder, FolderCompressorDeleteOptions deleteOptions = FC_DELETE_NONE);
 
     // Static method to register the QML type
     static void registerQml();
