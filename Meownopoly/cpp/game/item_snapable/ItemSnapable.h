ec3129c1bfac679369347b3a8b4ad0037029433b
Valere21 <valere.gh@hotmail.fr>
2025-12-23 06:51:51 +0100

    fix casse issue + minor stuff

--- Meownopoly/cpp/game/item_snapable/ItemSnapable.h
+++ Meownopoly/cpp/game/item_snapable/ItemSnapable.h
@@ -12,7 +12,7 @@
 #include <QJsonValueRef>
 
 #include "Displayparameter.h"
-#include "Decorationparameter.h"
+#include "decorationparameter.h"
 #include "polygonParameter.h"
 
 class ItemSnapable : public QObject
